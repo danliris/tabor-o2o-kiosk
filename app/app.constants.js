513bb2cf77e6f05578bf28c9f5fa4469e7fd9946
danliris <danliris.cloud@gmail.com>
2017-08-29 18:32:09 +0700

    Update app.constants.js

--- app/app.constants.js
+++ app/app.constants.js
@@ -1,6 +1,6 @@
 angular
 	.module('app')
 	.constant('Urls', {
-	    //BASE_API: 'http://tabor-o2o-webapi-internal-dev.azurewebsites.net/api',
-	     BASE_API: 'http://11.11.1.42:10010/api',
+	    BASE_API: 'http://tabor-o2o-webapi-internal-dev.azurewebsites.net/api',
+	    //BASE_API: 'http://11.11.1.42:10010/api',
 	});
